d4a9b1f3e36ceba26e96c9d84df6a6edd82e1046
songwoo <thddn191@gmail.com>
2021-05-22 14:45:21 +0900

    commit 3(~ch03-1 Query Method)

--- src/main/java/com/fastcampus/jpa/bookmanager/repository/UserRepository.java
+++ src/main/java/com/fastcampus/jpa/bookmanager/repository/UserRepository.java
@@ -3,6 +3,28 @@ package com.fastcampus.jpa.bookmanager.repository;
 import com.fastcampus.jpa.bookmanager.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     // spring jpa라이브러리가 지원해주는 영역 by extends <entity, pk type>
+
+
+    // User 해도 되지만, 여러 개면 오류가 나오니 List타입으로 만들자
+    //List<User> findByName(String name); // 이름을 통해 유저 가지고 오는 메소드
+    // Optinal<>, Set<>으로 해도 정상적으로 객체가 리턴된다.
+    // query에 대한 return타입은 고정적으로 사용 안 해도 된다.
+    Optional<User> findByName(String name);
+    User findByEmail(String email);
+    User getByEmail(String email);
+    User readByEmail(String email);
+    User queryByEmail(String email);
+    User searchByEmail(String email);
+    User streamByEmail(String email);
+    User findUserByEmail(String email);
+
+    // 숫자로 가져올 객체 수 정의
+    List<User> findFirst1ByName(String name);
+    List<User> findTop1ByName(String name);
+
 }
--- src/test/java/com/fastcampus/jpa/bookmanager/repository/UserRepositoryTest.java
+++ src/test/java/com/fastcampus/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -26,62 +26,30 @@ class UserRepositoryTest {
     @Test
     //@Transactional
     void crud() {
-//         save(...) + userRepository.flush(); // db반영 시점 조절, log상으로는 큰 변화 없다.
-//        userRepository.saveAndFlush(new User("new marint", "abc@naver.com"));
-//        userRepository.findAll().forEach(System.out::println);
-//
-//        long count = userRepository.count();
-//        System.out.println(count); // 5
-//
-//        boolean exists = userRepository.existsById(1L); // Long type의 1번 id값, count를 통해 불러옴
-//        System.out.println(exists);
+        //userRepository.save(new User("kim", "fa@naver.com"));
+        User user = userRepository.findById(1L).orElseThrow(RuntimeException::new);
+        user.setEmail("asda@naver.com");
+        userRepository.save(user);
 
-        // select로 존재하는지 확인 하고 delete 실햄시킴
-        // userRepository.delete(userRepository.findById(1L).orElseThrow(RuntimeException::new));
-        //userRepository.deleteById(1L);
+        userRepository.findAll().forEach(System.out::println);
+    }
 
-        // select하고, 각각의 record에 대해 delete메소드 수행
-        //userRepository.deleteAll();
-        // entity를 인자로 준다면 선택적으로 delete 가능
-        //userRepository.deleteAll(userRepository.findAllById(Lists.newArrayList(1L,3L)));
+    @Test
+    void select() {
+        System.out.println(userRepository.findByName("woosong"));
+        // 모두 동일한 쿼리, 동일한 결괏값을 가짐
+        // 잘못 전달될만한 네이밍 ex) findSomethingByEmail 같은 거 쓰면 안 된다.
+        // 잘못된 메소드이름은 실재 동작될 때 오류난다.
+        System.out.println("findByEmail" + userRepository.findByEmail("woosong@naver.com"));
+        System.out.println("getByEmail" + userRepository.getByEmail("woosong@naver.com"));
+        System.out.println("readByEmail" + userRepository.readByEmail("woosong@naver.com"));
+        System.out.println("queryByEmail" + userRepository.queryByEmail("woosong@naver.com"));
+        System.out.println("searchByEmail" + userRepository.searchByEmail("woosong@naver.com"));
+        System.out.println("streamByEmail" + userRepository.streamByEmail("woosong@naver.com"));
+        System.out.println("findUserByEmail" + userRepository.findUserByEmail("woosong@naver.com"));
 
-        // 하지만, entity의 갯수가 많아질 경우 각각 수행하기에 상당히 비효율적인 연산이 진행된다.
-        // 실행 시켜보면 인자로 준 쿼리만 실행되며, or 연산자를 사용해 entity를 삭제하는 것을 확인할 수 있다.
-        // userRepository.deleteInBatch(userRepository.findAllById(Lists.newArrayList(1L,3L)));
-
-        // delete 단 한번만 사용하는 쿼리
-        //userRepository.deleteAllInBatch();
-
-        //userRepository.findAll().forEach(System.out::println);
-
-//        // 페이징 처리(springframwork.domain.page) = data 그룹화.. 댓글 목록 생각하면 쉽다.
-//        Page<User> users = userRepository.findAll(PageRequest.of(1,3));
-//        System.out.println("page : "+ users);
-//        System.out.println("totalElements : "+ users.getTotalElements());
-//        System.out.println("totalPages : "+ users.getTotalPages());
-//        System.out.println("numberOfElemtents : "+ users.getNumberOfElements());
-//        System.out.println("sort : "+ users.getSort());
-//        System.out.println("size : "+ users.getSize());
-//
-//        users.getContent().forEach(System.out::println);
-
-
-        // where절과 like절의 구현, probe로 검색하기
-        // matcher로 한다면 where절만 구현하게 된다.
-        ExampleMatcher matcher = ExampleMatcher.matching()
-                .withIgnorePaths("name") // matching하는 곳에서 무시할 대상, name은 무시하지않겠다는 뜻
-                .withMatcher("email",endsWith());
-
-        Example<User> example = Example.of(new User("temp", "naver.com"),matcher) ;// probe 가짜 entity를 인자로 갖는다
-
-        // 양방향 like 검색
-        // 생각보다 많이 쓰이진 않는다.
-        User user = new User();
-        user.setEmail("11");
-        ExampleMatcher matcher1 = ExampleMatcher.matching().withMatcher("email", contains());
-        Example<User> example1 = Example.of(user, matcher1);
-        
-        userRepository.findAll(example1).forEach(System.out::println);
+        System.out.println("findTop1ByName" + userRepository.findTop1ByName("woosong@naver.com"));
+        System.out.println("findFirst1ByName" + userRepository.findFirst1ByName("woosong@naver.com"));
 
     }
 }
